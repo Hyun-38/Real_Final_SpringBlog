53f7378d49f480d12d4964f505bf4d1711019d1f
DJ-WALNUT <2044lava@gmail.com>
2025-01-21 17:47:38 +0900

    choi

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 # 최원서 합류
 # 야이 하하하하
 # hamham
+# 가나다라마바사
 # I'm last
 # 동스라이팅
 # I'm Jjin last
---
+++ src/test/java/catholic/mini/Board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package catholic.mini.Board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

df8ca6d5b3c3ad2d62287a610583cdc9f32d5044
kkkkkkkkkwon <ch80551@gmail.com>
2025-02-10 01:53:47 +0900

    8까지 완료

--- src/main/java/com/study/domain/post/PostController.java
+++ src/main/java/com/study/domain/post/PostController.java
@@ -5,7 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping; //@PostMapping ->> import class로 이거 추가됨
+import org.springframework.web.bind.annotation.RequestMethod; //RequestMethod ->> 오류나서 import class로 이 문장 추가
 import org.springframework.web.bind.annotation.RequestParam; //@RequestParam ->> import class로 이거 추가됨
+import com.study.common.dto.MessageDto; // 아래 MessageDto 오류 나서 일단 import문 추가
+
 
 import java.util.List; //List ->> import class로 이거 추가됨
 
@@ -25,12 +28,33 @@ public class PostController {
         return "post/write";
     }
 
+
     // 신규 게시글 생성
     @PostMapping("/post/save.do")
-    public String savePost(final PostRequest params) {
+    public String savePost(final PostRequest params, Model model) {
         postService.savePost(params);
-        return "redirect:/post/list.do";
+        MessageDto message = new MessageDto("게시글 생성이 완료되었습니다.", "/post/list.do", RequestMethod.GET, null);
+        return showMessageAndRedirect(message, model);
     }
+
+
+    // 기존 게시글 수정
+    @PostMapping("/post/update.do")
+    public String updatePost(final PostRequest params, Model model) {
+        postService.updatePost(params);
+        MessageDto message = new MessageDto("게시글 수정이 완료되었습니다.", "/post/list.do", RequestMethod.GET, null);
+        return showMessageAndRedirect(message, model);
+    }
+
+
+    // 게시글 삭제
+    @PostMapping("/post/delete.do")
+    public String deletePost(@RequestParam final Long id, Model model) {
+        postService.deletePost(id);
+        MessageDto message = new MessageDto("게시글 삭제가 완료되었습니다.", "/post/list.do", RequestMethod.GET, null);
+        return showMessageAndRedirect(message, model);
+    }
+
 
     // 게시글 리스트 페이지
     @GetMapping("/post/list.do")
@@ -48,11 +72,10 @@ public class PostController {
         return "post/view";
     }
 
-    // 기존 게시글 수정
-    @PostMapping("/post/update.do")
-    public String updatePost(final PostRequest params) {
-        postService.updatePost(params);
-        return "redirect:/post/list.do";
+    // 사용자에게 메시지를 전달하고, 페이지를 리다이렉트 한다.
+    private String showMessageAndRedirect(final MessageDto params, Model model) { //MessageDto 이거 빨간색 오류 뜸 -->> 일단 import문으로 가져왔는데 맞는지 모름
+        model.addAttribute("params", params);
+        return "common/messageRedirect";
     }
 
 }
